57ae51d794cbe9353d7e16e558b6027a6f692e58
DevServer <ey@hawkingbrothers.com>
2021-04-12 13:04:23 +0000

    Improvements in the UserFetcher class.

--- manager/src/ReadModel/User/UserFetcher.php
+++ manager/src/ReadModel/User/UserFetcher.php
@@ -17,6 +17,8 @@ class UserFetcher
     }
 
     /**
+     * Метод проверяет наличие пользователя по токену восстановления.
+     *
      * @param string $token
      * @return bool
      * @throws Exception
@@ -28,19 +30,22 @@ class UserFetcher
             ->from('user_users')
             ->where('reset_token_token = :token')
             ->setParameter(':token', $token)
-            ->execute()->fetchOne();
+            ->execute()
+            ->fetchOne();
 
         return (bool)$user;
     }
 
     /**
+     * Метод выполняет поиск пользователя по e-mail для авторизации.
+     *
      * @param string $email
      * @return AuthView|null
      * @throws Exception
      */
     public function findForAuthByEmail(string $email): ?AuthView
     {
-        $result = $this->connection->createQueryBuilder()
+        $user = $this->connection->createQueryBuilder()
             ->select([
                 'id',
                 'email',
@@ -52,15 +57,23 @@ class UserFetcher
             ->where('email = :email')
             ->setParameter('email', $email)
             ->execute()
-            ->fetchAllAssociative();
+            ->fetchAssociative();
 
-        if (!$user = array_shift($result)) {
+        if (!$user) {
             return null;
         }
 
         return AuthView::fromArray($user);
     }
 
+    /**
+     * Метод выполняет поиск пользователя по социальной сети для авторизации.
+     *
+     * @param string $network
+     * @param string $identity
+     * @return AuthView|null
+     * @throws Exception
+     */
     public function findForAuthByNetwork(string $network, string $identity): ?AuthView
     {
         $result = $this->connection->createQueryBuilder()
@@ -87,13 +100,15 @@ class UserFetcher
     }
 
     /**
+     * Метод выполняет поиск пользователя по e-mail.
+     *
      * @param string $email
      * @return ShortView|null
      * @throws Exception
      */
     public function findByEmail(string $email): ?ShortView
     {
-        $result = $this->connection->createQueryBuilder()
+        $user = $this->connection->createQueryBuilder()
             ->select([
                 'id',
                 'email',
@@ -104,9 +119,9 @@ class UserFetcher
             ->where('email = :email')
             ->setParameter('email', $email)
             ->execute()
-            ->fetchAllAssociative();
+            ->fetchAssociative();
 
-        if (!$user = array_shift($result)) {
+        if (!$user) {
             return null;
         }
 
