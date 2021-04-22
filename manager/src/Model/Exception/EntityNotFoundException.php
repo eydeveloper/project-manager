039227dd332c7eaf5202ab5858e20d85a5279302
LocalServer <ey@hawkingbrothers.com>
2021-04-22 16:56:47 +0300

    Replaced EntityNotFoundException, documented UserRepository class.

---
+++ manager/src/Model/Exception/EntityNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Exception;
+
+class EntityNotFoundException extends \DomainException
+{
+}
--- manager/src/Model/User/Entity/User/UserRepository.php
+++ manager/src/Model/User/Entity/User/UserRepository.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace App\Model\User\Entity\User;
 
-use App\Model\EntityNotFoundException;
+use App\Model\Exception\EntityNotFoundException;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Doctrine\Persistence\ObjectRepository;
 
 class UserRepository
 {
     private EntityManagerInterface $entityManager;
-    private EntityRepository $repository;
+    private ObjectRepository $repository;
 
     public function __construct(EntityManagerInterface $entityManager)
     {
@@ -19,22 +21,34 @@ class UserRepository
         $this->repository = $entityManager->getRepository(User::class);
     }
 
+    /**
+     * Поиск пользователя по токену подтверждения регистрации.
+     *
+     * @param string $token Токен подтверждения регистрации.
+     * @return User|null
+     */
     public function findByConfirmToken(string $token): ?User
     {
-        /** @var User $user */
-        $user = $this->repository->findOneBy(['confirmToken' => $token]);
-
-        return $user;
+        return $this->repository->findOneBy(['confirmToken' => $token]);
     }
 
+    /**
+     * Поиск пользователя по токену восстановления пароля.
+     *
+     * @param string $token Токен восстановления пароля.
+     * @return User|null
+     */
     public function findByResetToken(string $token): ?User
     {
-        /** @var User $user */
-        $user = $this->repository->findOneBy(['resetToken.token' => $token]);
-
-        return $user;
+        return $this->repository->findOneBy(['resetToken.token' => $token]);
     }
 
+    /**
+     * Получение пользователя по идентификатору.
+     *
+     * @param Id $id Идентификатор.
+     * @return User
+     */
     public function get(Id $id): User
     {
         /** @var $user User */
@@ -45,9 +59,14 @@ class UserRepository
         return $user;
     }
 
+    /**
+     * Получение пользователя по электронной почте.
+     *
+     * @param Email $email Электронная почта.
+     * @return User
+     */
     public function getByEmail(Email $email): User
     {
-        /** @var $user User */
         if (!$user = $this->repository->findOneBy(['email' => $email->getValue()])) {
             throw new EntityNotFoundException('Пользователь не найден.');
         }
@@ -55,28 +74,50 @@ class UserRepository
         return $user;
     }
 
+    /**
+     * Проверка существования пользователя по электронной почте.
+     *
+     * @param Email $email Электронная почта.
+     * @return bool
+     * @throws NoResultException
+     * @throws NonUniqueResultException
+     */
     public function hasByEmail(Email $email): bool
     {
         return $this->repository->createQueryBuilder('u')
-            ->select('COUNT(u.id)')
-            ->andWhere('u.email = :email')
-            ->setParameter(':email', $email->getValue())
-            ->getQuery()
-            ->getSingleScalarResult() > 0;
+                ->select('COUNT(u.id)')
+                ->andWhere('u.email = :email')
+                ->setParameter(':email', $email->getValue())
+                ->getQuery()
+                ->getSingleScalarResult() > 0;
     }
 
+    /**
+     * Проверка существования пользователя по социальной сети.
+     *
+     * @param string $network Название социальной сети.
+     * @param string $identity Идентификатор пользователя в социальной сети.
+     * @return bool
+     * @throws NoResultException
+     * @throws NonUniqueResultException
+     */
     public function hasByNetworkIdentity(string $network, string $identity): bool
     {
         return $this->repository->createQueryBuilder('u')
-            ->select('COUNT(u.id)')
-            ->innerJoin('u.networks', 'n')
-            ->andWhere('n.network = :network AND n.identity = :identity')
-            ->setParameter(':network', $network)
-            ->setParameter(':identity', $identity)
-            ->getQuery()
-            ->getSingleScalarResult() > 0;
+                ->select('COUNT(u.id)')
+                ->innerJoin('u.networks', 'n')
+                ->andWhere('n.network = :network AND n.identity = :identity')
+                ->setParameter(':network', $network)
+                ->setParameter(':identity', $identity)
+                ->getQuery()
+                ->getSingleScalarResult() > 0;
     }
 
+    /**
+     * Добавление пользователя.
+     *
+     * @param User $user
+     */
     public function add(User $user): void
     {
         $this->entityManager->persist($user);
